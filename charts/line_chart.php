de6f91a7a49e16415e54d250d164f79021a18021
Rezwan Anjum Nibir <rezwananjum@gmail.com>
2018-05-16 00:09:42 +0600

    chart.js and conditional table

---
+++ charts/line_chart.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+	<script src="js/line_script.js"></script>
+</head>
+<body>
+<div class="chart-container">
+    <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script src="js/line_script.js"></script>
+</body>
+</html>
